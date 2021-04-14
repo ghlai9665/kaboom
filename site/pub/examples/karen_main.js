e79ef2d33b5bb8c4eefeade5acfb9c56a4075a55
Gary Lai <glai9665@gmail.com>
2021-04-14 17:52:54 +0800

    modify collides to allow shooting in any direction

--- site/pub/examples/karen_main.js
+++ site/pub/examples/karen_main.js
@@ -12,50 +12,46 @@ export const karenMain = () => {
 
     const p1 = add([pos(0, 0), rect(24, 24), solid()]);
 
-    const p2 = add([pos(100, 100), rect(24, 24), solid(), "enemy"]);
+    const spawnEnemy = () => {
+      add([pos(100, 100), rect(36, 36), solid(), "enemy"]);
+    };
+
+    loop(1, () => {
+      spawnEnemy();
+    });
 
     const SPEED = 240;
-
-    keyDown("w", () => {
-      p1.move(0, -SPEED);
-    });
-
-    keyDown("s", () => {
-      p1.move(0, SPEED);
-    });
-
-    keyDown("a", () => {
-      p1.move(-SPEED, 0);
-    });
-
-    keyDown("d", () => {
-      p1.move(SPEED, 0);
-    });
+    let direction = "up";
 
     keyDown("up", () => {
       p1.move(0, -SPEED);
+      direction = "up";
     });
 
     keyDown("down", () => {
       p1.move(0, SPEED);
+      direction = "down";
     });
 
     keyDown("left", () => {
       p1.move(-SPEED, 0);
+      direction = "left";
     });
 
     keyDown("right", () => {
       p1.move(SPEED, 0);
+      direction = "right";
     });
 
-    keyPress(["space", "up"], () => {
-      spawnBullet(p1.pos.sub(4, 0));
-      spawnBullet(p1.pos.add(4, 0));
+    keyPress("space", () => {
+      spawnBullet(p1.pos.sub(4, 0), direction);
+      spawnBullet(p1.pos.add(4, 0), direction);
     });
 
-    let BULLET_SPEED = 50;
+    let BULLET_SPEED = 100;
+
     // run this callback every frame for all objects with tag "bullet"
-    action("bullet", (b) => {
+    action("bulletUp", (b) => {
       b.move(0, -BULLET_SPEED);
       // remove the bullet if it's out of the scene for performance
       if (b.pos.y < 0) {
@@ -63,20 +59,28 @@ export const karenMain = () => {
       }
     });
 
-    collides("bullet", "enemy", (b, e) => {
-      destroy(b);
-      destroy(e);
-      score.value += 1;
-      score.text = score.value;
+    action("bulletDown", (b) => {
+      b.move(0, BULLET_SPEED);
     });
 
-    p1.action(() => {
-      p1.resolve();
+    action("bulletLeft", (b) => {
+      b.move(-BULLET_SPEED, 0);
     });
 
-    p2.action(() => {
-      p2.resolve();
+    action("bulletRight", (b) => {
+      b.move(BULLET_SPEED, 0);
     });
+
+    collides(
+      ["bulletUp", "bulletDown", "bulletLeft", "bulletRight"],
+      "enemy",
+      (b, e) => {
+        destroy(b);
+        destroy(e);
+        score.value += 1;
+        score.text = score.value;
+      }
+    );
 
     keyPress("f1", () => {
       kaboom.debug.showArea = !kaboom.debug.showArea;
@@ -85,13 +89,17 @@ export const karenMain = () => {
   });
 };
 
-function spawnBullet(p) {
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function spawnBullet(p, direction) {
+  const capitalizedDirection = capitalizeFirstLetter(direction);
   add([
     rect(2, 6),
     pos(p),
     origin("center"),
     color(0.5, 0.5, 1),
-    // strings here means a tag
-    "bullet",
+    `bullet${capitalizedDirection}`,
   ]);
 }
